5e48afe95b12766eea525aca6eed8061d6af1e4c
nticea <nticea@stanford.edu>
2022-09-23 22:31:02 -0700

    clean up and starting new times run for 48Nx_2Ny

--- outputs/48Nx_2Ny_0.125doping/no_phonons.jl
+++ outputs/48Nx_2Ny_0.125doping/no_phonons.jl
@@ -11,7 +11,7 @@ ITensors.set_warn_order(50)
 ## PARAMETERS ## 
 
 # Model 
-Nx=8
+Nx=48
 Ny=2
 yperiodic=true
 
@@ -24,21 +24,21 @@ Upd=0
 Upp=3
 Udd=8
 doping=0.125
-ω=0#0.5
-g0pp=0#0.1
-g0dd=0#0.1
-g1pd=0#0.1
-g1dp=0#0.1
-g1pp=0#0.1
+ω=0
+g0pp=0
+g0dd=0
+g1pd=0
+g1dp=0
+g1pp=0
 max_phonons=0 # (n+1)*4 = total site dimension 
 
 # DMRG parameters 
-DMRG_numsweeps = 20 # total number of iterations 
-DMRG_maxdim = 64
+DMRG_numsweeps = 80 # total number of iterations 
+DMRG_maxdim = 2048
 DMRG_cutoff = 1E-10
 
 ## SAVE OUT INFO ##
-DMRG_numsweeps_per_save = 5 # If don't want to save regularly, just set this to DMRG_numsweeps
+DMRG_numsweeps_per_save = 3 # If don't want to save regularly, just set this to DMRG_numsweeps
 println("Running DMRG...")
 dmrg_run(Nx, Ny, yperiodic, μ, εd, εp, 
         tpd, tpp, Upd, Upp, Udd, ω, g0pp, g0dd, g1pd, 
--- outputs/48Nx_2Ny_0.125doping/phonons.jl
+++ outputs/48Nx_2Ny_0.125doping/phonons.jl
@@ -5,6 +5,9 @@ include(joinpath(@__DIR__,"../../../src/model.jl"))
 include(joinpath(@__DIR__,"../../../src/utilities.jl"))
 include(joinpath(@__DIR__,"../../../src/run.jl"))
 
+# The equilibrium corrs require contracting tensors w many indices
+ITensors.set_warn_order(50)
+
 ## PARAMETERS ## 
 
 # Model 
@@ -15,31 +18,38 @@ yperiodic=true
 μ=0
 εd=0
 εp=3
-tpd=-1
-tpp=-0.5
+tpd=1
+tpp=0.5
 Upd=0
 Upp=3
 Udd=8
 doping=0.125
-ω=0.5
-g0pp=0.1
-g0dd=0.1
+ω=0.1
+g0pp=0.01
+g0dd=0.01
 g1pd=0
 g1dp=0
 g1pp=0
-max_phonons=4 # (n+1)*4 = total site dimension 
+max_phonons=3 # (n+1)*4 = total site dimension 
 
 # DMRG parameters 
-DMRG_numsweeps = 60 # total number of iterations 
+DMRG_numsweeps = 80 # total number of iterations 
 DMRG_maxdim = 2048
 DMRG_cutoff = 1E-10
 
 ## SAVE OUT INFO ##
-DMRG_numsweeps_per_save = 1 # If don't want to save regularly, just set this to DMRG_numsweeps
+DMRG_numsweeps_per_save = 3 # If don't want to save regularly, just set this to DMRG_numsweeps
 println("Running DMRG...")
 dmrg_run(Nx, Ny, yperiodic, μ, εd, εp, 
         tpd, tpp, Upd, Upp, Udd, ω, g0pp, g0dd, g1pd, 
         g1dp, g1pp, doping, max_phonons, DMRG_numsweeps,
         DMRG_maxdim, DMRG_cutoff, DMRG_numsweeps_per_save;
         disk_save=true,
+        dir_path=@__DIR__)
+
+println("Computing correlations...")
+correlations_run(Nx, Ny, yperiodic, μ, εd, εp, 
+        tpd, tpp, Upd, Upp, Udd, ω, g0pp, g0dd, g1pd, 
+        g1dp, g1pp, doping, max_phonons, DMRG_numsweeps,
+        DMRG_maxdim, DMRG_cutoff, DMRG_numsweeps_per_save;
         dir_path=@__DIR__)
