3bc93c20770f3364a05773cc1fce14cfd0ef3353
nticea <nticea@stanford.edu>
2023-01-31 10:43:56 -0800

    last push before checkpointing the eqcorr calc

--- outputs/gB1/visualize.jl
+++ outputs/gB1/visualize.jl
@@ -1,9 +1,9 @@
 using Pkg
-Pkg.activate(joinpath(@__DIR__,"../.."))
-include(joinpath(@__DIR__,"../../src/model.jl"))
-include(joinpath(@__DIR__,"../../src/plotting.jl"))
-include(joinpath(@__DIR__,"../../src/utilities.jl"))
-include(joinpath(@__DIR__,"../../src/sites/site_hubbholst.jl"))
+Pkg.activate(joinpath(@__DIR__, "../.."))
+include(joinpath(@__DIR__, "../../src/model.jl"))
+include(joinpath(@__DIR__, "../../src/plotting.jl"))
+include(joinpath(@__DIR__, "../../src/utilities.jl"))
+include(joinpath(@__DIR__, "../../src/sites/site_hubbholst.jl"))
 using ITensors
 using ITensors.HDF5
 
@@ -14,7 +14,7 @@ using ITensors.HDF5
 # 0.25
 #loadpath = "/Users/nicole/Dropbox/Grad school/Devereaux lab/Hubbard model/3BHPhonons/ThreeBandHubbardPhonons.jl/outputs/gB1/16Nx_2Ny_3εp_1tpd_0.5tpp_0Vpd_3Upp_8Udd_0.125doping_1ωB1_0ωA1_1gB1_0gA1_results.h5"
 # 0.025 
-loadpath = "/Users/nicole/Dropbox/Grad school/Devereaux lab/Hubbard model/3BHPhonons/ThreeBandHubbardPhonons.jl/outputs/gB1/16Nx_2Ny_3εp_1tpd_0.5tpp_0Vpd_3Upp_8Udd_0.125doping_0.1ωB1_0ωA1_0.1gB1_0gA1_results.h5"
+#loadpath = "/Users/nicole/Dropbox/Grad school/Devereaux lab/Hubbard model/3BHPhonons/ThreeBandHubbardPhonons.jl/outputs/gB1/16Nx_2Ny_3εp_1tpd_0.5tpp_0Vpd_3Upp_8Udd_0.125doping_0.1ωB1_0ωA1_0.1gB1_0gA1_results.h5"
 # 0.0025
 #loadpath = "/Users/nicole/Dropbox/Grad school/Devereaux lab/Hubbard model/3BHPhonons/ThreeBandHubbardPhonons.jl/outputs/gB1/16Nx_2Ny_3εp_1tpd_0.5tpp_0Vpd_3Upp_8Udd_0.125doping_1ωB1_0ωA1_0.1gB1_0gA1_results.h5"
 # 0.00025
@@ -24,7 +24,7 @@ loadpath = "/Users/nicole/Dropbox/Grad school/Devereaux lab/Hubbard model/3BHPho
 
 # Flags 
 do_fit = true
-do_save = true 
+do_save = true
 do_eq_corr = true
 
 ## LOAD RESULTS ##
@@ -42,7 +42,7 @@ plotd = plot_densities(dmrg_results)
 #λ = calculate_λ(params)
 #@show λ
 
-if do_save 
+if do_save
     if do_eq_corr
         savefig(ploteq, "equilibrium_correlations.png")
     end
--- src/plotting.jl
+++ src/plotting.jl
@@ -4,39 +4,39 @@ using FFTW
 using NPZ
 using Statistics
 using CurveFit
-include(joinpath(@__DIR__,"model.jl"))
+include(joinpath(@__DIR__, "model.jl"))
 
 ## EQUILIBRIUM CORRELATIONS ##
 
-function plot_densities(dmrg_results::Union{DMRGResults,DMRGResultsMinimal};title::String="")
+function plot_densities(dmrg_results::Union{DMRGResults,DMRGResultsMinimal}; title::String="")
     p1 = plot_charge_density(dmrg_results)
     p2 = plot_spin_density(dmrg_results)
     p3 = plot_phonon_density(dmrg_results)
-    plot(p1, p2, p3, 
-            layout=Plots.grid(1,3, widths=(1/3,1/3,1/3)), size=(2000,500), title=title)
+    plot(p1, p2, p3,
+        layout=Plots.grid(1, 3, widths=(1 / 3, 1 / 3, 1 / 3)), size=(2000, 500), title=title)
 end
 
 function plot_charge_density(dmrg_results::Union{DMRGResults,DMRGResultsMinimal})
     n = dmrg_results.charge_density'
-    n = n[1:end-2,:] # remove the last rung
-    p = plot(1:length(n[1:3:end,1]), n[1:3:end,:], label="py")
-    plot!(1:length(n[2:3:end,1]), n[2:3:end,:], label="d")
-    plot!(1:length(n[3:3:end,1]), n[3:3:end,:], label="px")
+    n = n[1:end-2, :] # remove the last rung
+    p = plot(1:length(n[1:3:end, 1]), n[1:3:end, :], label="py")
+    plot!(1:length(n[2:3:end, 1]), n[2:3:end, :], label="d")
+    plot!(1:length(n[3:3:end, 1]), n[3:3:end, :], label="px")
     ylabel!("⟨n⟩")
     xlabel!("Site")
     title!("Electron density")
-    return p 
+    return p
 end
 
 function plot_phonon_density(dmrg_results::Union{DMRGResults,DMRGResultsMinimal}, mode::Int; ylims=nothing)
-    n = dmrg_results.phonon_density[:,:,mode]
+    n = dmrg_results.phonon_density[:, :, mode]
     _plot_phonon_density(n', ylims=ylims)
 end
 
 function plot_phonon_density(dmrg_results::Union{DMRGResults,DMRGResultsMinimal}; ylims=nothing)
-    
+
     function has_nonzero_elements(a)
-        nonzero_inds = findall(x -> x>0, a)
+        nonzero_inds = findall(x -> x > 0, a)
         return length(nonzero_inds) > 0
     end
 
@@ -45,7 +45,7 @@ function plot_phonon_density(dmrg_results::Union{DMRGResults,DMRGResultsMinimal}
     # find the modes with a nonzero number of phonons 
     toplot = zeros(size(n)[1:2])
     for m in 1:nmodes
-        mode = n[:,:,m]
+        mode = n[:, :, m]
         if has_nonzero_elements(mode)
             toplot += mode
         end
@@ -55,55 +55,55 @@ function plot_phonon_density(dmrg_results::Union{DMRGResults,DMRGResultsMinimal}
 end
 
 function _plot_phonon_density(n::AbstractArray; ylims=nothing)
-    n = n[1:end-2,:] # remove the last rung
-    p = plot(1:length(n[1:3:end,1]), n[1:3:end,:], label="py")
-    plot!(1:length(n[2:3:end,1]), n[2:3:end,:], label="d")
-    plot!(1:length(n[3:3:end,1]), n[3:3:end,:], label="px")
+    n = n[1:end-2, :] # remove the last rung
+    p = plot(1:length(n[1:3:end, 1]), n[1:3:end, :], label="py")
+    plot!(1:length(n[2:3:end, 1]), n[2:3:end, :], label="d")
+    plot!(1:length(n[3:3:end, 1]), n[3:3:end, :], label="px")
     if !isnothing(ylims)
-        ylims!((ylim[0],ylim[1]))
+        ylims!((ylim[0], ylim[1]))
     end
     ylabel!("⟨nb⟩")
     xlabel!("Site")
     title!("Phonon density")
-    return p 
+    return p
 end
 
 function plot_spin_density(dmrg_results::Union{DMRGResults,DMRGResultsMinimal}; ylims=nothing)
     n = dmrg_results.spin_density'
-    n = n[1:end-2,:] # remove the last rung
-    p = plot(1:length(n[1:3:end,1]), n[1:3:end,:], label="py")
-    plot!(1:length(n[2:3:end,1]), n[2:3:end,:], label="d")
-    plot!(1:length(n[3:3:end,1]), n[3:3:end,:], label="px")
+    n = n[1:end-2, :] # remove the last rung
+    p = plot(1:length(n[1:3:end, 1]), n[1:3:end, :], label="py")
+    plot!(1:length(n[2:3:end, 1]), n[2:3:end, :], label="d")
+    plot!(1:length(n[3:3:end, 1]), n[3:3:end, :], label="px")
     if !isnothing(ylims)
-        ylims!((ylim[0],ylim[1]))
+        ylims!((ylim[0], ylim[1]))
     end
     ylabel!("⟨Sz⟩")
     xlabel!("Site")
     title!("Spin density")
-    return p 
+    return p
 end
 
 function square_residual(y, ỹ)
-    sum((ỹ .- y).^2)
+    sum((ỹ .- y) .^ 2)
 end
 
 function exponential_fit(x, y)
-    a,b = exp_fit(x, y)
-    fit_y = a.*exp.(b.*x)
+    a, b = exp_fit(x, y)
+    fit_y = a .* exp.(b .* x)
     err = square_residual(fit_y, y)
-    return a,b,fit_y,err
+    return a, b, fit_y, err
 end
 
 function power_law_fit(x, y)
-    a,b = power_fit(x, y)
-    fit_y = a.*(x.^b)
+    a, b = power_fit(x, y)
+    fit_y = a .* (x .^ b)
     err = square_residual(fit_y, y)
-    return a,b,fit_y,err
-end 
+    return a, b, fit_y, err
+end
 
 function compare_fits(x, y)
-    exp_a, exp_b, exp_fit, exp_err  = exponential_fit(x,y)
-    power_a, power_b, power_fit, power_err = power_law_fit(x,y)
+    exp_a, exp_b, exp_fit, exp_err = exponential_fit(x, y)
+    power_a, power_b, power_fit, power_err = power_law_fit(x, y)
     if power_err < exp_err
         return "power-law fit", power_a, power_b, power_fit
     else
@@ -120,10 +120,10 @@ function _plot_equilibrium_correlations(corrs, corrtype; do_fit=false, fit_type=
         if isnothing(fit_type)
             type, a, b, fit = compare_fits(xrange, abs.(corrs))
         else
-            if fit_type=="exponential"
+            if fit_type == "exponential"
                 a, b, fit, _ = exponential_fit(xrange, abs.(corrs))
                 type = "exponential fit"
-            elseif fit_type=="power"
+            elseif fit_type == "power"
                 a, b, fit, _ = power_law_fit(xrange, abs.(corrs))
                 type = "power-law fit"
             else
@@ -133,7 +133,7 @@ function _plot_equilibrium_correlations(corrs, corrtype; do_fit=false, fit_type=
     end
 
     plot(log10.(xrange), log10.(abs.(corrs)))
-    title!(corrtype*"-"*corrtype*" correlation")
+    title!(corrtype * "-" * corrtype * " correlation")
     xlabel!("Distance from centre site (log10)")
     ylabel!("Correlation (log10)")
     plot!(log10.(xrange), log10.(fit), label=type)
@@ -147,10 +147,10 @@ function _plot_equilibrium_correlations_fitted(corrs, corrtype; fit_type=nothing
     if isnothing(fit_type)
         type, a, b, fit = compare_fits(xrange, abs.(corrs))
     else
-        if fit_type=="exponential"
+        if fit_type == "exponential"
             a, b, fit, _ = exponential_fit(xrange, abs.(corrs))
             type = "exponential fit"
-        elseif fit_type=="power"
+        elseif fit_type == "power"
             a, b, fit, _ = power_law_fit(xrange, abs.(corrs))
             type = "power-law fit"
         else
@@ -159,7 +159,7 @@ function _plot_equilibrium_correlations_fitted(corrs, corrtype; fit_type=nothing
     end
 
     plot(log10.(xrange), log10.(abs.(corrs)))
-    title!(corrtype*"-"*corrtype*" correlation", fontsize=8)
+    title!(corrtype * "-" * corrtype * " correlation", fontsize=8)
     if corrtype == "dSC_dydy"
         xlabel!("Distance from centre site (log10)")
     end
@@ -174,7 +174,7 @@ function _plot_equilibrium_correlations(corrs, corrtype)
     xrange = collect(1:length(corrs))
 
     p = plot(log10.(xrange), log10.(abs.(corrs)), fontsize=8)
-    title!(corrtype*"-"*corrtype*" correlation")
+    title!(corrtype * "-" * corrtype * " correlation")
     if corrtype == "dSC_dydy"
         xlabel!("Distance from centre site (log10)")
     end
@@ -182,11 +182,11 @@ function _plot_equilibrium_correlations(corrs, corrtype)
         ylabel!("Correlation (log10)")
     end
 
-    return p 
+    return p
 end
 
-function plot_equilibrium_correlations(eq_corrs::EquilibriumCorrelations; 
-                                    do_fit=true, fit_type=nothing)
+function plot_equilibrium_correlations(eq_corrs::EquilibriumCorrelations;
+    do_fit=true, fit_type=nothing)
     if do_fit
         p1 = _plot_equilibrium_correlations_fitted(eq_corrs.spin, "spin", fit_type=fit_type)
         p2 = _plot_equilibrium_correlations_fitted(eq_corrs.charge, "charge", fit_type=fit_type)
@@ -206,28 +206,28 @@ function plot_equilibrium_correlations(eq_corrs::EquilibriumCorrelations;
         p7 = _plot_equilibrium_correlations(eq_corrs.dSC_pypx, "dSC_pypx")
         p8 = _plot_equilibrium_correlations(eq_corrs.dSC_py1px2, "dSC_py1px2")
     end
-    plot(p1, p2, p3, p4, p5, p6, p7, p8, 
-            layout=Plots.grid(2,4, widths=(1/4,1/4,1/4,1/4,1/4,1/4,1/4,1/4)), size=(2000,800))
+    plot(p1, p2, p3, p4, p5, p6, p7, p8,
+        layout=Plots.grid(2, 4, widths=(1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4, 1 / 4)), size=(2000, 800))
 end
 
-function plot_equilibrium_correlations(eq_corrs::EquilibriumCorrelations, 
-                                        corrtype::String; do_fit=true, fit_type=nothing)
+function plot_equilibrium_correlations(eq_corrs::EquilibriumCorrelations,
+    corrtype::String; do_fit=true, fit_type=nothing)
 
-    if corrtype=="spin"
+    if corrtype == "spin"
         corrs = eq_corrs.spin
-    elseif corrtype=="charge"
+    elseif corrtype == "charge"
         corrs = eq_corrs.charge
-    elseif corrtype=="dSC_dxdx"
+    elseif corrtype == "dSC_dxdx"
         corrs = eq_corrs.dSC_dxdx
-    elseif corrtype=="dSC_dpx"
+    elseif corrtype == "dSC_dpx"
         corrs = eq_corrs.dSC_dpx
-    elseif corrtype=="dSC_dydy"
+    elseif corrtype == "dSC_dydy"
         corrs = eq_corrs.dSC_dydy
-    elseif corrtype=="dSC_pyd"
+    elseif corrtype == "dSC_pyd"
         corrs = eq_corrs.dSC_pyd
-    elseif corrtype=="dSC_pypx"
+    elseif corrtype == "dSC_pypx"
         corrs = eq_corrs.dSC_pypx
-    elseif corrtype=="dSC_py1px2"
+    elseif corrtype == "dSC_py1px2"
         corrs = eq_corrs.dSC_py1px2
     else
         @error "Invalid correlation type"
@@ -245,8 +245,8 @@ end
 function plot_entropy(tebd_results::TEBDResults)
     ent = tebd_results.entropy
     niters = size(ent)[2]
-    ϕ_entropy = ent[1,:]
-    ψ_entropy = ent[2,:]
+    ϕ_entropy = ent[1, :]
+    ψ_entropy = ent[2, :]
     plot(1:niters, ϕ_entropy, label="ϕ(t)")
     plot!(1:niters, ψ_entropy, label="ψ(t)")
     title!("Von Neumann Entropy")
@@ -260,7 +260,7 @@ end
 ## TIME-DEPENDENT CORRELATIONS ## 
 
 function plot_correlation_function(tebd_results::TEBDResults)
-    heatmap(LinearAlgebra.norm.(tebd_results.corrs'),c=:heat)
+    heatmap(LinearAlgebra.norm.(tebd_results.corrs'), c=:heat)
     title!("Correlation function")
     xlabel!("Site")
     ylabel!("Time")
@@ -271,44 +271,44 @@ function make_spectral_fcn(corrs, p::Parameters; left_offset::Int=0)
     f2 = fftshift(fft(p.τ * reverse(corrs, dims=1))')
 
     qs = 2 * π * fftshift(fftfreq(p.N, 1))
-    ωs = 2 * π * fftshift(fftfreq(size(corrs)[2], 1/p.τ))
+    ωs = 2 * π * fftshift(fftfreq(size(corrs)[2], 1 / p.τ))
 
     # Compensate for midpoint 
     sqw = zeros(size(f1))
-    for i in 1:p.N 
-        sqw[:,i] = imag.(exp.(1im * qs[i] * (p.mid-left_offset)) * f1[:,i])
-                    + imag.(exp.(1im * qs[i] * (p.N-1-p.mid-left_offset)) * f2[:,i])
+    for i in 1:p.N
+        sqw[:, i] = imag.(exp.(1im * qs[i] * (p.mid - left_offset)) * f1[:, i])
+        +imag.(exp.(1im * qs[i] * (p.N - 1 - p.mid - left_offset)) * f2[:, i])
     end
 
     # If working with an even number of sites, must average across midline
     if iseven(p.N)
-        sqw = (reverse(sqw, dims=2) + sqw) ./2
+        sqw = (reverse(sqw, dims=2) + sqw) ./ 2
     end
 
-    return real.(sqw)/π, ωs, qs 
+    return real.(sqw) / π, ωs, qs
 end
 
-function decay(u;shape="exponential")
-    if shape=="exponential"
+function decay(u; shape="exponential")
+    if shape == "exponential"
         t = collect(1:(length(u)))
-        λ = 0.1/sqrt(length(t))
-        v = exp.(-λ*t)
+        λ = 0.1 / sqrt(length(t))
+        v = exp.(-λ * t)
     else
         @error "Not implemented"
     end
     u .* v
 end
 
-function plot_spectral_function(tebd_results::TEBDResults, p::Parameters; 
-                                smooth_signal=true, lims=nothing)
+function plot_spectral_function(tebd_results::TEBDResults, p::Parameters;
+    smooth_signal=true, lims=nothing)
     corrs = tebd_results.corrs # num_time_steps x num_sites
     N, U, t, ω, g0, g1 = p.N, p.U, p.t, p.ω, p.g0, p.g1
     # Optionally convolve raw time data with decaying exponential?
     if smooth_signal
         # decaying exponential
-        corrs = reverse(hcat(decay.(eachrow(corrs))...)',dims=2)
+        corrs = reverse(hcat(decay.(eachrow(corrs))...)', dims=2)
         # pad with zeros 
-        corrs = hcat(corrs, zeros(size(corrs))) 
+        corrs = hcat(corrs, zeros(size(corrs)))
     end
     # Calculate the spectral function 
     ff, ωs, qs = make_spectral_fcn(corrs, p)
@@ -316,9 +316,9 @@ function plot_spectral_function(tebd_results::TEBDResults, p::Parameters;
     # Zoom in to the relevant bit 
     if isnothing(lims)
         nstep = size(corrs)[2]
-        lims = (floor(Int, nstep/2) - floor(Int, 0.07*nstep),floor(Int, nstep/2) + floor(Int, 0.07*nstep))
+        lims = (floor(Int, nstep / 2) - floor(Int, 0.07 * nstep), floor(Int, nstep / 2) + floor(Int, 0.07 * nstep))
     end
-    ff = ff[lims[1]:lims[2],:]
+    ff = ff[lims[1]:lims[2], :]
     ωs = ωs[lims[1]:lims[2]]
 
     # Plot 
@@ -330,12 +330,12 @@ function plot_spectral_function(tebd_results::TEBDResults, p::Parameters;
 end
 
 function plot_spectral_function_slice(tebd_results::TEBDResults, p::Parameters; slice=0)
-    findnearest(A::AbstractArray,t) = findmin(abs.(A.-t))[2]
-    
+    findnearest(A::AbstractArray, t) = findmin(abs.(A .- t))[2]
+
     ff, ωs, qs = make_spectral_fcn(tebd_results.corrs, p)
-    ω = findnearest(ωs,slice)
-    scatter(qs, abs.(ff[ω,:]),color="blue", label=nothing)
-    plot!(qs, abs.(ff[ω,:]),color="blue", label=nothing)
+    ω = findnearest(ωs, slice)
+    scatter(qs, abs.(ff[ω, :]), color="blue", label=nothing)
+    plot!(qs, abs.(ff[ω, :]), color="blue", label=nothing)
     title!("Slice of S(q,ω) at ω=$slice")
 end
 
@@ -344,8 +344,8 @@ function compare_to_ED(tebd_results::TEBDResults, p::Parameters)
     corrs = tebd_results.corrs
     ED_corrs = npzread("/Users/nicole/Dropbox/Grad school/Devereaux lab/ITensors.jl/examples/dmrg/ed.npy")
     numsteps = size(corrs)[2]
-    plot(1:numsteps, real.(corrs[4,:]), label="DMRG real part")
+    plot(1:numsteps, real.(corrs[4, :]), label="DMRG real part")
     plot!(1:numsteps, real.(ED_corrs[1:numsteps]), label="ED real part")
-    plot!(1:numsteps, imag.(corrs[4,:]), label="DMRG complex part")
+    plot!(1:numsteps, imag.(corrs[4, :]), label="DMRG complex part")
     plot!(1:numsteps, imag.(ED_corrs[1:numsteps]), label="ED complex part")
 end
